14781300ca9cb35adc1b86b7f6ca5b4f25ca5566
yukke9265 <yukke9265@gmail.com>
2026-05-25 01:14:23 +0900

    add melter recipe

--- src/main/java/com/yukke9265/cobblestone_xx_compressed/datagen/recipe/CobblestoneMelterRecipeDatagen.java
+++ src/main/java/com/yukke9265/cobblestone_xx_compressed/datagen/recipe/CobblestoneMelterRecipeDatagen.java
@@ -2,15 +2,64 @@ package com.yukke9265.cobblestone_xx_compressed.datagen.recipe;
 
 import com.yukke9265.cobblestone_xx_compressed.registry.ModBlocks;
 import com.yukke9265.cobblestone_xx_compressed.registry.ModFluids;
+import com.yukke9265.cobblestone_xx_compressed.registry.ModItems;
 
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.fluids.FluidStack;
+import net.minecraft.world.item.Items;
 
 public final class CobblestoneMelterRecipeDatagen {
     private static final MelterRecipeDefinition[] RECIPES = new MelterRecipeDefinition[] {
-        new MelterRecipeDefinition("tier_topaz_compressed_cobblestone_to_molten_tier_topaz_compressed_cobblestone", ModBlocks.TierCompressedCobblestone.TOPAZ.getBlock().get(), new FluidStack(ModFluids.TierMoltenCompressedCobblestone.TOPAZ.getFluidEntry().getStillFluid().get(), 1000), 6400, 64),
-        new MelterRecipeDefinition("tier_diamond_compressed_cobblestone_to_molten_tier_diamond_compressed_cobblestone", ModBlocks.TierCompressedCobblestone.DIAMOND.getBlock().get(), new FluidStack(ModFluids.TierMoltenCompressedCobblestone.DIAMOND.getFluidEntry().getStillFluid().get(), 1000), 51200, 512)
+        new MelterRecipeDefinition(
+            "topaz_mixture_to_glowtopaz_fluid",
+             ModItems.TOPAZ_MIXTURES.get(),
+            new FluidStack(ModFluids.WaterBasedFluid.GLOW_TOPAZ.getFluidEntry().getStillFluid().get(), 1000),
+             26214400,
+             4096
+        ),
+        new MelterRecipeDefinition(
+            "ruby_mixture_to_redruby_fluid",
+             ModItems.RUBY_MIXTURES.get(),
+            new FluidStack(ModFluids.WaterBasedFluid.RED_RUBY.getFluidEntry().getStillFluid().get(), 1000),
+            26214400,
+            4096
+        ),
+        new MelterRecipeDefinition(
+            "sapphire_mixture_to_bluesapphire_fluid",
+             ModItems.SAPPHIRE_MIXTURE.get(),
+            new FluidStack(ModFluids.WaterBasedFluid.SHINY_SAPPHIRE.getFluidEntry().getStillFluid().get(), 1000),
+             26214400,
+             4096
+        ),
+        new MelterRecipeDefinition(
+            "blaze_mixture_to_blaze_fluid",
+             ModItems.BLAZE_MIXTURES.get(),
+            new FluidStack(ModFluids.WaterBasedFluid.SHINY_BLAZE.getFluidEntry().getStillFluid().get(), 1000),
+             26214400,
+             4096
+        ),
+        new MelterRecipeDefinition(
+            "emerald_mixture_to_unstable_emerald_fluid",
+             ModItems.EMERALD_MIXTURES.get(),
+            new FluidStack(ModFluids.WaterBasedFluid.UNSTABLE_EMERALD.getFluidEntry().getStillFluid().get(), 1000),
+             26214400,
+             4096
+        ),
+        new MelterRecipeDefinition(
+            "ancient_mixture_to_molten_netherite",
+             ModItems.ANCIENT_MIXTURES.get(),
+            new FluidStack(ModFluids.WaterBasedFluid.MOLTEN_NETHERITE.getFluidEntry().getStillFluid().get(), 1000),
+             26214400,
+             4096
+        ),
+        new MelterRecipeDefinition(
+            "enderpearl_to_molten_ender",
+             Items.ENDER_PEARL,
+            new FluidStack(ModFluids.WaterBasedFluid.MOLTEN_ENDER.getFluidEntry().getStillFluid().get(), 500),
+             26214400,
+             4096
+        )
     };
 
     private CobblestoneMelterRecipeDatagen() {
